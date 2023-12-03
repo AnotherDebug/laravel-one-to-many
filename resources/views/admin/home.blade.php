61614616516138214a27035fb2197715f2c19645
AnotherDebug <133654225+AnotherDebug@users.noreply.github.com>
2023-12-03 10:36:08 +0100

    Initial commit

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<div class="adminHome">
+    <h1 class="m-3 text-center">Home Dashboard</h1>
+</div>
+
+@endsection
+
+@section('title')
+    | Dashboard
+@endsection
---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.guest')
+
+
+@section('content')
+
+<div class="guestHome">
+    <h1>Homepage pubblica</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat molestias consequuntur eaque quaerat eos sint ex, explicabo illum laboriosam hic dignissimos aliquid perferendis nostrum saepe expedita quasi beatae voluptas autem cum excepturi rerum, tempora nulla. Nemo fuga, ex quo ullam ducimus hic quae. Sunt reprehenderit sed odio est nesciunt impedit eligendi voluptate voluptates consectetur quam. Quia enim provident error dolore saepe excepturi sunt deserunt vitae neque animi iste tenetur ratione voluptates nihil culpa tempore, perferendis unde consequatur aut quis natus. Nobis, repellat repudiandae? Incidunt nihil vero a, aliquid voluptatum dolorem tempora facere blanditiis sapiente nisi laboriosam nesciunt dignissimos quia vitae.</p>
+</div>
+
+@endsection
