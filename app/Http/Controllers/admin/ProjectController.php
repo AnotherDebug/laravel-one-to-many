f1619b64e61e46689659f99921d5875120abb1ed
AnotherDebug <antonio.faddanno@gmail.com>
2023-12-03 16:50:38 +0100

    [FIX]: query types in typeProjects function

--- app/Http/Controllers/admin/ProjectController.php
+++ app/Http/Controllers/admin/ProjectController.php
@@ -8,6 +8,7 @@ use App\Models\Project;
 use App\Http\Requests\StoreProjectRequest;
 use App\Functions\Helper;
 use Illuminate\Support\Facades\Storage;
+use App\Models\Type;
 
 class ProjectController extends Controller
 {
@@ -24,8 +25,8 @@ class ProjectController extends Controller
 
     public function typeProjects()
     {
-        $projects = Project::all();
-        return view('admin.projects.type-projects', compact('projects'));
+        $types = Type::all();
+        return view('admin.projects.type-projects', compact('types'));
     }
     /**
      * Show the form for creating a new resource.
